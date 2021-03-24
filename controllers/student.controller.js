843d6d4e9fb2c869bd12136172aef036020aac47
Atif Aziz Memon <73272104+atifm1726@users.noreply.github.com>
2021-03-25 02:22:20 +0500

    Update student.controller.js

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,10 +1,49 @@
 //Atif Aziz Memon 1812105 controller
-const student=require('../models/student.model')
+const Student=require('../models/student.model')
+
+
 
 
 exports.test=(req,res)=>{
     res.send('Uploaded')
-}
+};
 
-exports.student_create=(req,res)=>{}
 
+exports.student_create=(req, res)=>{
+    let student= new Student({
+        name:req.body.name,
+        age:req.body.age
+    });
+
+    student.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Student Created successfully')
+    })
+}; 
+exports.student_details=(req, res)=>{
+    Student.findById(req.params.id, (err, student)=>{
+        if(err)
+        return next(err);
+        res.send(student);
+
+    })
+
+
+
+};
+exports.student_update=(req, res)=>{
+    Student.findByIdAndUpdate(req.params.id, {$set: req.body}, (err, student)=>{
+        if (err) return next(err);
+        res.send('Updated');
+
+    })
+};
+
+exports.student_delete=(req,res)=>{
+    Student.findByIdAndRemove(req.params.id, (err)=>{
+        if (err) return next(err);
+        res.send('Deleted');
+    })
+};
