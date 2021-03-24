242094108d174fa772bb07d166104066b65c79ac
Atif Aziz Memon <73272104+atifm1726@users.noreply.github.com>
2021-03-25 02:27:30 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 //Atif Aziz Memon 1812105
+const mongoose  = require('mongoose');
 const express=require('express');
-const bodyParser=require('body-parser');
-const app=express();
+const bodyParser = require('body-parser');
 const student=require('./routes/student.route');
 
 
-app.use('/student',student)
+const app=express();
 
-app.listen(8080,()=>{console.log('Server is running on Port 8080')});
+mongoose.connect("mongodb://localhost/Student",{useNewUrlParser:true, useUnifiedTopology:true}).
+then(
+    ()=>{
+        console.log('DB Connected')
+    }
+).catch(err=>{
+    console.log("Cant connect",err)
+    process.exit();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use('/students',student);
+
+
+app.listen(3000,()=>{console.log('Server is running')});
+//http:localhost:3000/student
+
 
 //Atif Aziz Memon
